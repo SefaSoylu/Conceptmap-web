6f4dd619e00ed33ffef713c078a767cdd4241e52
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2025-05-06 20:39:51 +1000

    Comment existing code, use new home page and navigation bar component

--- concept-map-web/src/App.js
+++ concept-map-web/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import ConceptMapperForm from './pages/ConceptMapperForm';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <ConceptMapperForm />
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <ConceptMapperForm />
-      </header>
+      <NavigationBar />
+      <HomePage />
     </div>
   );
 }
