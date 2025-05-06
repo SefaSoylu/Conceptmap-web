3a94d23827cb4cfa748d6565e8f732ba6b821285
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2025-05-06 23:06:31 +1000

    Add dummy text for testing, create skeleton of new UI layout

--- concept-map-web/src/pages/ConceptMapperPage/ConceptMapperPage.js
+++ concept-map-web/src/pages/ConceptMapperPage/ConceptMapperPage.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import styles from './ConceptMapperPage.module.scss';
 
 const ConceptMapperPage = () => {
+  const [toggle, setToggle] = useState(false);
+  const [results, setResults] = useState(false);
+
   return (
     <div className={styles.wrapper}>
-      <div>
-        <p>Input section</p>
+      <div className={styles.inputs}>
+        <button onClick={() => setToggle(!toggle)}>
+          {toggle ? 'Open' : 'Close'}
+        </button>
+        {toggle ? (
+          <h2 className={styles.inputs__closed__heading}>Inputs</h2>
+        ) : (
+          <>
+            <h2>Inputs</h2>
+            <p>Inputs section closed</p>
+          </>
+        )}
       </div>
-      <div className={styles.results_wrapper}>
-        <p>Results section</p>
+      <div className={styles.results}>
+        <h2>Results</h2>
+        {results ? (
+          <p>Results shown.</p>
+        ) : (
+          <p>Results will appear here upon successful concept mapping.</p>
+        )}
       </div>
     </div>
   );
